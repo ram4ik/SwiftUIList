2d0894fbc4e4f85046252bedf0e0bd0cf582e66f
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-21 22:33:00 +0200

    init

--- SwiftUIList/ContentView.swift
+++ SwiftUIList/ContentView.swift
@@ -8,9 +8,28 @@
 
 import SwiftUI
 
+// To dynamically load the list with views
+struct listCell {
+    var name: String
+    var num: Int
+}
+
 struct ContentView: View {
+    
+    var listCells = [
+        listCell(name: "First", num: 1),
+        listCell(name: "Second", num: 2),
+        listCell(name: "Third", num: 3),
+        listCell(name: "Fourth", num: 4)
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        List(listCells, id: \.name) { listCell in
+            VStack {
+                Text(listCell.name)
+                Text("\(listCell.num)")
+            }
+        }
     }
 }
 
